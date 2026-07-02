6f14a4641dfbc2b9c4354b0274bddc3a9956a059
Sunny Fung <newfish@gmail.com>
2026-07-02 19:01:18 +0800

    fix: isolate backfill tool flags

--- tools/backfill_legacy_pin_alias/main.go
+++ tools/backfill_legacy_pin_alias/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"man-p2p/common"
 	"man-p2p/man"
 )
 
 func main() {
-	configPath := flag.String("config", "./config.toml", "path to config.toml")
-	chainName := flag.String("chain", "mvc", "chain to backfill legacy pin aliases for")
-	limit := flag.Int("limit", 0, "maximum aliases to create; 0 means no limit")
-	flag.Parse()
+	flags := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	configPath := flags.String("config", "./config.toml", "path to config.toml")
+	chainName := flags.String("chain", "mvc", "chain to backfill legacy pin aliases for")
+	limit := flags.Int("limit", 0, "maximum aliases to create; 0 means no limit")
+	flags.Parse(os.Args[1:])
 
+	os.Args = []string{os.Args[0]}
 	common.InitConfig(*configPath)
 	man.InitRuntime(*chainName, common.Db, common.TestNet, "0", true)
 	defer func() {
